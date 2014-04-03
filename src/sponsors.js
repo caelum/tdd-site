4f39480100378d92ddf70bcb53175429e7af8dfe
Artur Diniz Adam <artur.daz@gmail.com>
2014-04-03 16:18:53 -0300

    default sponsors logo

--- src/sponsors.js
+++ src/sponsors.js
@@ -1,13 +1,13 @@
 var sponsors = [
 	{	name: "Alura",
 		website: "http://www.alura.com.br",
-		img: "img/alura_logo.svg",
+		img: "img/alura-logo.png",
 		description: "Cursos online de tecnologia"
 	},
 
 	{	name: "Caelum",
 		website: "http://www.caelum.com.br",
-		img: "img/caelum_logo.svg",
+		img: "img/caelum-logo.png",
 		description: [
 			{content: "Curso de Java", url: "http://www.caelum.com.br/cursos/java/"},
 			{content: "Curso de Android", url: "http://www.caelum.com.br/curso-android/"},
@@ -20,7 +20,7 @@ var sponsors = [
 
 	{ name: "Casa do Código",
 		website: "http://www.casadocodigo.com.br",
-		img: "img/cdc_logo.svg",
+		img: "img/cdc-logo.png",
 		description: "Conheça também os livros da Casa do Código"
 	}
 ]
