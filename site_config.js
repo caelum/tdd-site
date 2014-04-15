b0b025b367a9a559a21d3dd37a5a210601f3a214
Mauricio Aniche <mauricioaniche@gmail.com>
2014-04-15 17:10:22 -0300

    o certo eh apontar pro site da caelum

--- site_config.js
+++ site_config.js
@@ -3,7 +3,7 @@ var conf = {
 	company: {
 		name: 'Caelum',
 		img: 'img/main-logo.svg',
-		website: 'http://www.tddnomundoreal.com.br'
+		website: 'http://www.caelum.com.br'
 	},
 	participants: require('./src/participants.js'),
 	sponsors: require('./src/sponsors.js')
