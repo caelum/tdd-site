a3b9855415284d0be039e7a512eab8fec41f4be0
Mauricio Aniche <mauricioaniche@gmail.com>
2014-04-17 17:00:40 -0300

    descricao

--- site_config.js
+++ site_config.js
@@ -2,7 +2,7 @@ var conf = {
 
 	title: 'TDD',
 
-	description: 'Write about anything',
+	description: 'Test-Driven Development (TDD) é uma das mais populares práticas ágeis de desenvolvimento de software. Mas com certeza uma das quais as pessoas mais tem dúvidas. A ideia deste site é explicar TDD para não-técnicos. Quais as vantagens? Serei mais produtivo? Essas e outras perguntas são respondidas aqui.',
 
 	url: 'http://tdd.caelum.com.br/',
 
